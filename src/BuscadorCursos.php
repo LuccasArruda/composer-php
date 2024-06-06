060a8f4cc2de31121d06fbf7c7203f2b334614ef
luccas-arruda <luccasarruda@hotmail.com.br>
2024-06-05 23:35:45 -0300

    feat: adicionar arquivos de autoload

---
+++ src/BuscadorCursos.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Alura\BuscadorCursos;
+
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class BuscadorCursos{
+    private Crawler $crawler;
+    private ClientInterface $client;
+
+    public function __construct(ClientInterface $httpClient, Crawler $crawler){
+        $this->crawler = $crawler;
+        $this->client = $httpClient;
+    }
+
+    public function buscar(string $url): array{
+        $resposta = $this->client->request('GET', $url);
+
+        $html = $resposta->getBody();
+
+        $this->crawler->addHtmlContent($html);
+
+        $elementosCursos = $this->crawler->filter('span.card-curso__nome');
+        $cursos = [];
+        foreach($elementosCursos as $elemento){
+            $cursos[] = $elemento->textContent;
+        }
+
+        return $cursos;
+    }
+}
